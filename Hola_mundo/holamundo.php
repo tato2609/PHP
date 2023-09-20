5593f156dad26de313a5c07740179bbde3432cb3
Javier <tatoesquivia26@gmail.com>
2023-09-20 06:26:29 -0500

    first commit

---
+++ Hola_mundo/holamundo.php
@@ -0,0 +1,10 @@
+<?php
+    echo "Hola Mundo"."<br>";
+    $numero1 = 6;
+    $numero2 =7;
+    echo "los dos numeros son" . $numero1 . $numero2;
+    for ( $i = 0; $i < $numero1; $i++) {
+        echo " " . $numero1+$numero2;
+
+    }
+?>
